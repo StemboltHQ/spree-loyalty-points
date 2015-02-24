4ddeb4ac4adfd7f386a3e922728ed4f5b2e3360d
Kyle Macey <kyle@wholelattelove.com>
2015-02-24 14:16:30 -0500

    fix indentation remove unnecessary tests

--- spec/controllers/spree/admin/general_settings_controller_decorator_spec.rb
+++ spec/controllers/spree/admin/general_settings_controller_decorator_spec.rb
@@ -10,21 +10,7 @@ describe Spree::Admin::GeneralSettingsController do
     controller.stub(:authorize!).and_return(true)
     controller.stub(:authorize_admin).and_return(true)
   end
-
-  describe "set_loyalty_points_settings callback" do
-
-    it "should be included in before action callbacks" do
-      # @KyleMacey - fix this shiznit
-      Spree::Admin::GeneralSettingsController._process_action_callbacks.select{ |callback| callback.kind == :before }.map(&:filter).include?(:set_loyalty_points_settings).should be_truthy
-    end
-
-    it "should have only option set to [:edit]" do
-      ([:edit] - Spree::Admin::GeneralSettingsController._process_action_callbacks.select{ |callback| callback.filter == :set_loyalty_points_settings }.first.options[:only]).should be_empty
-      binding.pry
-    end
-
-  end
-
+  
   describe "GET 'edit'" do
 
     it "assigns @preferences_loyalty_points" do
